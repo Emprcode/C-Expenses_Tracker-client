04cde9df67dc42fe87f0952f539e3f473a5617ba
Narayan Aryal <aryal.coding@gmail.com>
2023-01-07 14:18:44 +1100

    Code completed

--- src/helpers/axiosHelpers.js
+++ src/helpers/axiosHelpers.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = "http://localhost:8000/api/v1";
+const baseURL = process.env.NODE_EN !== "production" ? "http://localhost:8000/api/v1" : "/api/v1"
 const userAPI = baseURL + "/user";
 const transAPI = baseURL + "/transaction";
 
